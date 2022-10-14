70f87cfea6030c22de756ebfe049aef595ba0e9b
retop56 <retop56@gmail.com>
2022-10-14 02:58:28 -0700

    Add some styling and change square generation

--- js/script.js
+++ js/script.js
@@ -1,17 +1,35 @@
+const titleHeading = document.createElement("h1");
+titleHeading.classList.add("title");
+titleHeading.textContent = "Etch-A-Sketch";
+document.body.append(titleHeading);
+
+const wrapper = document.createElement("div");
+wrapper.classList.add("sc-wrapper");
+document.body.append(wrapper);
+
 const squareContainer = document.createElement("div");
 squareContainer.classList.add("square-container");
-document.body.append(squareContainer);
+wrapper.append(squareContainer);
 
-for (let i = 0; i < 256; i++) {
-  let square = document.createElement("div");
-  square.classList.add("square", `square-num-${i + 1}`);
-  squareContainer.append(square);
+let numOfSquares = 16;
+
+function generateSquares(num) {
+  for (let i = 0; i < num ** 2; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square", `square-num-${i + 1}`);
+    squareContainer.append(square);
+  }
 }
 
-// const squareChildren = squareContainer.childNodes;
-// for (let i = 0; i < squareChildren.length; i++) {
-//   child = squareChildren[i];
-//   child.addEventListener("mouseenter", (event) => {
-//     event.target.style.backgroundColor = "red";
-//   });
-// }
+function addHoverEffect() {
+  const squareChildren = squareContainer.childNodes;
+  for (let i = 0; i < squareChildren.length; i++) {
+    child = squareChildren[i];
+    child.addEventListener("mouseenter", (event) => {
+      event.target.style.backgroundColor = "red";
+    });
+  }
+}
+
+generateSquares(numOfSquares);
+addHoverEffect();
