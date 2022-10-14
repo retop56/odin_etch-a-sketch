9fc324cd0182f0d33c30bd9f76d08bed6fddb65d
retop56 <retop56@gmail.com>
2022-10-14 02:07:48 -0700

    Add hover effect

--- js/script.js
+++ js/script.js
@@ -4,6 +4,14 @@ document.body.append(squareContainer);
 
 for (let i = 0; i < 256; i++) {
   let square = document.createElement("div");
-  square.classList.add(`square-num-${i + 1}`);
+  square.classList.add("square", `square-num-${i + 1}`);
   squareContainer.append(square);
 }
+
+// const squareChildren = squareContainer.childNodes;
+// for (let i = 0; i < squareChildren.length; i++) {
+//   child = squareChildren[i];
+//   child.addEventListener("mouseenter", (event) => {
+//     event.target.style.backgroundColor = "red";
+//   });
+// }
