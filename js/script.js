390a4916bb58b9e99973ec1d517cdc21c5f547be
retop56 <retop56@gmail.com>
2022-10-14 03:43:00 -0700

    Add dynamic square generation logic

--- js/script.js
+++ js/script.js
@@ -11,12 +11,45 @@ const squareContainer = document.createElement("div");
 squareContainer.classList.add("square-container");
 wrapper.append(squareContainer);
 
-let numOfSquares = 16;
+const changeNumOfSquaresButton = document.createElement("button");
+changeNumOfSquaresButton.textContent = "Change Number Of Squares";
+document.body.append(changeNumOfSquaresButton);
+changeNumOfSquaresButton.setAttribute("onclick", "askNumOfSquares()");
+
+function askNumOfSquares() {
+  while (true) {
+    const newNumOfSquares = parseInt(
+      prompt("How many squares would you like? (min 1, max 100)", "1"),
+      10
+    );
+    if (newNumOfSquares === null) {
+      return;
+    }
+    if (typeof newNumOfSquares !== "number") {
+      alert("Please enter a number!");
+    } else if (newNumOfSquares > 100 || newNumOfSquares < 1) {
+      alert("Must be number between 1 and 100!");
+    } else {
+      clearOutSquares();
+      generateSquares(newNumOfSquares);
+      addHoverEffect();
+      break;
+    }
+  }
+}
+
+function clearOutSquares() {
+  while (squareContainer.firstChild) {
+    squareContainer.removeChild(squareContainer.firstChild);
+  }
+}
 
 function generateSquares(num) {
   for (let i = 0; i < num ** 2; i++) {
     let square = document.createElement("div");
     square.classList.add("square", `square-num-${i + 1}`);
+    square.style.width = `${600 / num}px`;
+    square.style.height = `${600 / num}px`;
     squareContainer.append(square);
   }
 }
@@ -31,5 +64,5 @@ function addHoverEffect() {
   }
 }
 
-generateSquares(numOfSquares);
+generateSquares(16);
 addHoverEffect();
